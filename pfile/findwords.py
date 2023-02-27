04d70a43f26a37d328dc62777405c62642bc5564
VidyaSagarKomhar <123475184+VidyaSagarKomhar@users.noreply.github.com>
2023-02-27 21:17:32 +0530

    Update findwords.py

--- pfile/findwords.py
+++ pfile/findwords.py
@@ -1,10 +1,6 @@
 from google.cloud import storage
 
-def read_file_words(bucket_name,file_path):
-    storage_client = storage.Client()
-    bucket = storage_client.get_bucket(bucket_name)
-    blob = bucket.blob(file_path)
-    read_each_file = blob.download_as_text(encoding="utf-8")
-    words = read_each_file.split()
+def read_file_words(datastring):
+    words = datastring.split()
     print('The len of the files {}'.format(len(words)))
     return len(words)
